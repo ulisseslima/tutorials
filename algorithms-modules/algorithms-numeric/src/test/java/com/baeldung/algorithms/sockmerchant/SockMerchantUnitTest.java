5dd8d769d97f5e85947dfc1f7db97e9f3bd3b381
Nikhil Bhargava <nikhil1.bhargava@ril.com>
2026-04-21 11:24:17 +0530

    BAEL-9641: Revising the Sock Merchant Problem in Java

--- algorithms-modules/algorithms-numeric/src/test/java/com/baeldung/algorithms/sockmerchant/SockMerchantUnitTest.java
+++ algorithms-modules/algorithms-numeric/src/test/java/com/baeldung/algorithms/sockmerchant/SockMerchantUnitTest.java
@@ -10,8 +10,7 @@ public class SockMerchantUnitTest {
     public void givenSockArray_whenUsingArray_thenReturnsCorrectPairCount() {
         SockMerchant merchant = new SockMerchant();
         int[] socks = {11, 22, 22, 11, 33, 3, 33, 111111, 33, 222222};
-        int colorMax = Arrays.stream(socks).max().getAsInt();
-        colorMax += 1;
+        int colorMax = 222223;
         int actualPairs = merchant.countPairsWithArray(socks.length, socks, colorMax);
         assertEquals(3, actualPairs);
     }
